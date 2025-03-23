07199fa210d34a82eb4ede11c6e8cdf918452fbf
Helios Qiu <admin@lovevup.com>
2025-03-23 21:52:13 +0800

    feat: 增加随机图片指令

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.core.message.components import At, Image
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+
+@register("random_img", "昼阳Helios", "一个随机图片插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
-    async def terminate(self):
-        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
+    @filter.command("img")
+    async def helloworld(self, event: AstrMessageEvent):
+        '''获取随机图片''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        chain = [
+            At(qq=event.get_sender_id()),  # At 消息发送者
+            Image.fromURL("https://pic.static.cv/api/random")  # 从本地文件目录发送图片
+        ]
+        yield event.chain_result(chain)
