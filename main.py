c0435936438e364224d46b16b7804e82b212d5d5
Helios Qiu <admin@lovevup.com>
2025-03-25 07:32:29 +0800

    feat:增加随机参数

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.core.message.components import At, Image
@@ -11,8 +13,13 @@ class MyPlugin(Star):
     @filter.command("img")
     async def helloworld(self, event: AstrMessageEvent):
         '''获取随机图片''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        random_number = random.random()
+        if random_number > 0.5:
+            param = '?orientation=landscape'
+        else:
+            param = '?orientation=portrait'
         chain = [
             At(qq=event.get_sender_id()),  # At 消息发送者
-            Image.fromURL("https://pic.static.cv/api/random")  # 从本地文件目录发送图片
+            Image.fromURL(f"https://pic.static.cv/api/random{param}")
         ]
         yield event.chain_result(chain)
